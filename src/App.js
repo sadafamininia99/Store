c1c699acc38c0f95314609e7e30438954190aeb7
sadafamininia99 <aminicomputer96@gmail.com>
2021-10-29 14:48:40 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
